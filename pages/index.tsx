c85a527681a037cff924db00981cd6d40e186c03
Andrew Keturi <andrewketuri@gmail.com>
2022-12-20 17:29:40 -0800

    Add prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
-import { Box } from '@chakra-ui/react'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "../styles/Home.module.css";
+import { Box } from "@chakra-ui/react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -17,5 +17,5 @@ export default function Home() {
       </Head>
       <Box>Hello Chakra</Box>
     </>
-  )
+  );
 }
