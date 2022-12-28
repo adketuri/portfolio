9012b3769478d6deba342f99b387b0000551aec6
Andrew Keturi <andrewketuri@gmail.com>
2022-12-28 11:56:26 -0800

    Update resume/opengraph tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,19 @@ import theme from "../styles/theme";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta property="og:url" content="https://andrew.alcuria.net" />
+        <meta property="og:title" content="Andrew Keturi - Portfolio" />
+        <meta
+          property="og:description"
+          content="Portfolio of software engineer Andrew Keturi"
+        />
+        <meta
+          property="og:image"
+          content="https://andrew.alcuria.net/images/me.png"
+        />
+        <meta property="og:site_name" content="Andrew Keturi - Portfolio" />
+      </Head>
 
       <body>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
